a371f01024256398db144cde962ee6c07df68785
Petr Messner <petr.messner@gmail.com>
2021-12-01 13:58:25 +0100

    aoc 2021 day 01 minor improvement

--- 2021/01.py
+++ 2021/01.py
@@ -1,4 +1,5 @@
 measurements = [int(line) for line in open('01_input.txt')]
+
 increments = 0
 for n, value in enumerate(measurements):
     if n > 0:
@@ -7,9 +8,7 @@ for n, value in enumerate(measurements):
 print('Increments:', increments)
 
 increases = 0
-for n in range(len(measurements) + 1):
-    if n >= 4:
-        if sum(measurements[n-3:n]) > sum(measurements[n-4:n-1]):
-            increases += 1
-
+for n in range(len(measurements) - 3):
+    if sum(measurements[n+1:n+4]) > sum(measurements[n:n+3]):
+        increases += 1
 print('Sliding window increases:', increases)
