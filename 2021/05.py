d44fcea75ae7ffaf0806f8727af2fe553ed2975b
Petr Messner <petr.messner@gmail.com>
2021-12-05 06:17:38 +0100

    aoc 2021 day 5 part 2

--- 2021/05.py
+++ 2021/05.py
@@ -23,12 +23,41 @@ for line in data:
         for i in rrange(x1, x2):
             over[(i, y1)] += 1
 
-print(over)
-
 p1 = sum(1 for pos, n in over.items() if n >= 2)
-print(p1)
+#print(p1)
 
-# For now, only consider horizontal and vertical lines: lines where either x1 = x2 or y1 = y2.
+del over, p1
+
+over2 = Counter()
+
+def sign(n):
+    if n < 0:
+        return -1
+    if n > 0:
+        return 1
+    assert 0
+
+for line in data:
+    m = re.match(r'^(\d+),(\d+) -> (\d+),(\d+)$' , line)
+    x1, y1, x2, y2 = m.groups()
+    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+
+    if x1 == x2:
+        for i in rrange(y1, y2):
+            over2[(x1, i)] += 1
+    elif y1 == y2:
+        for i in rrange(x1, x2):
+            over2[(i, y1)] += 1
+    elif abs(x1 - x2) == abs(y1 - y2):
+        for i in range(abs(x1 - x2) + 1):
+            print(i, (x1, y1), (x2, y2), (x1 + i * sign(x2 - x1), y1 + i * sign(y2 - y1))  )
+            over2[ (x1 + i * sign(x2 - x1), y1 + i * sign(y2 - y1)) ] += 1
+
+
+p2 = sum(1 for pos, n in over2.items() if n >= 2)
+print(p2)
+
+
 
 
 
