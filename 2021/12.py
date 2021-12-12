123b68f0d5d62756d40be9aa9386440a1412eb2e
Petr Messner <petr.messner@gmail.com>
2021-12-12 18:13:29 +0100

    aoc 2021 day 12 part 2

--- 2021/12.py
+++ 2021/12.py
@@ -11,7 +11,7 @@ def part1(input_data):
     edges = defaultdict(list)
     for line in input_data.splitlines():
         a, b = line.split('-')
-        print(f'  {a} -> {b}')
+        #print(f'  {a} -> {b}')
         edges[a].append(b)
         edges[b].append(a)
     assert edges['start']
@@ -24,7 +24,7 @@ def part1(input_data):
             assert isinstance(path, tuple)
             last_node = path[-1]
             for next_node in edges[last_node]:
-                print(repr(path), repr(next_node))
+                #print(repr(path), repr(next_node))
                 new_path = path + (next_node, )
                 if next_node == 'end':
                     all_paths.add(new_path)
@@ -33,8 +33,8 @@ def part1(input_data):
                 else:
                     new_front.append(new_path)
         front = new_front
-    print(len(all_paths))
-    pprint(all_paths)
+    #print(len(all_paths))
+    #pprint(all_paths)
     return len(all_paths)
 
 
@@ -89,3 +89,46 @@ sample_input_2 = dedent('''\
 assert part1(sample_input_2) == 226
 
 print('Part 1:', part1(open('12_input.txt').read()))
+
+def part2(input_data):
+    '''
+    How many paths through this cave system are there that visit small caves at most once?
+    '''
+    edges = defaultdict(list)
+    for line in input_data.splitlines():
+        a, b = line.split('-')
+        print(f'  {a} -> {b}')
+        edges[a].append(b)
+        edges[b].append(a)
+    assert edges['start']
+    assert edges['end']
+    front = [(('start', ), None)]
+    all_paths = set()
+    while front:
+        new_front = []
+        for path, twice in front:
+            assert isinstance(path, tuple)
+            last_node = path[-1]
+            for next_node in edges[last_node]:
+                #print(repr(path), repr(next_node))
+                new_path = path + (next_node, )
+                if next_node == 'start':
+                    continue
+                elif next_node == 'end':
+                    all_paths.add(new_path)
+                elif twice and small(next_node) and next_node in path:
+                    continue
+                elif not twice and small(next_node) and next_node in path:
+                    new_front.append((new_path, next_node))
+                else:
+                    new_front.append((new_path, twice))
+        front = new_front
+    print(len(all_paths))
+    pprint(list(enumerate(all_paths)))
+    return len(all_paths)
+
+assert part2(sample_input_0) == 36
+assert part2(sample_input_1) == 103
+assert part2(sample_input_2) == 3509
+
+print('Part 2:', part2(open('12_input.txt').read()))
